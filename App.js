b1ca7a551de884285c290d6a52704653d8679a54
rks1713 <161999099+rks1713@users.noreply.github.com>
2024-07-01 10:15:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+  Routes
+} from 'react-router-dom';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Home from './Components/Home';
+import './styles.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
